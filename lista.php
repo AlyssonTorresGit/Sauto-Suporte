646c38653cdc01042098450208384279941b070f
Alysson Torres <alysson-b@hotmail.com>
2025-04-04 16:18:26 -0300

    rty

--- lista.php
+++ lista.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sauto Suporte</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="lista.css">
 </head>
 <body>
     <main class="erros">
